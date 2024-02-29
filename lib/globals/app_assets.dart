e7d7c6da3d872d9109ea4686a9b960b14baa42c8
jayanandhanan <145838513+jayanandhanan@users.noreply.github.com>
2024-02-29 14:32:57 +0530

    Update app_assets.dart

--- lib/globals/app_assets.dart
+++ lib/globals/app_assets.dart
@@ -4,8 +4,8 @@ class AppAssets {
   static String linkedIn = 'assets/images/in.png';
   static String insta = 'assets/images/insta.png';
   static String twitter = 'assets/images/twit.png';
-  static String profile1 = 'assets/images/pro1.png';
-  static String profile2 = 'assets/images/pro2.png';
+  static String profile1 = 'assets/images/pro1.jpeg';
+  static String profile2 = 'assets/images/pro1.jpeg';
   static String code = 'assets/images/coding.png';
   static String brush = 'assets/images/brush-stroke.png';
   static String analyst = 'assets/images/analytics.png';
