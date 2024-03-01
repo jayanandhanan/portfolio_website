c70f46e268a218109de5b0d463e079e410555389
jayanandhanan <145838513+jayanandhanan@users.noreply.github.com>
2024-03-01 14:14:43 +0530

    Update home_page.dart

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -8,6 +8,14 @@ import 'package:my_portfolio/globals/app_text_styles.dart';
 import 'package:my_portfolio/globals/constants.dart';
 import 'package:my_portfolio/helper%20class/helper_class.dart';
 import 'package:my_portfolio/widgets/profile_animation.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse(
+    'https://drive.google.com/file/d/17GWNgaHmGp1DEEFeXmXyJkG5OWvbSJGO/view?usp=drive_link');
+final Uri _linkedInUrl = Uri.parse(
+    'https://www.linkedin.com/in/jaya-nandhanan-v-26392b258');
+final Uri _githubUrl = Uri.parse('https://github.com/jayanandhanan');
+final Uri _instagramUrl = Uri.parse('https://www.instagram.com/jaya_nandhanan/');
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,44 +26,42 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final socialButtons = <String>[
-    AppAssets.facebook,
-    AppAssets.twitter,
     AppAssets.linkedIn,
     AppAssets.insta,
     AppAssets.github,
   ];
 
-  var socialBI;
-
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return HelperClass(
-      mobile: Column(
-        children: [
-          buildHomePersonalInfo(size),
-          Constants.sizedBox(height: 25.0),
-          const ProfileAnimation()
-        ],
+    return SingleChildScrollView(
+      child: HelperClass(
+        mobile: Column(
+          children: [
+            buildHomePersonalInfo(size),
+            Constants.sizedBox(height: 25.0),
+            const ProfileAnimation()
+          ],
+        ),
+        tablet: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(child: buildHomePersonalInfo(size)),
+            const ProfileAnimation(),
+          ],
+        ),
+        desktop: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(child: buildHomePersonalInfo(size)),
+            const ProfileAnimation(),
+          ],
+        ),
+        paddingWidth: size.width * 0.1,
+        bgColor: Colors.transparent,
       ),
-      tablet: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Expanded(child: buildHomePersonalInfo(size)),
-          const ProfileAnimation(),
-        ],
-      ),
-      desktop: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Expanded(child: buildHomePersonalInfo(size)),
-          const ProfileAnimation(),
-        ],
-      ),
-      paddingWidth: size.width * 0.1,
-      bgColor: Colors.transparent,
     );
   }
 
@@ -95,13 +101,14 @@ class _HomePageState extends State<HomePage> {
                     textStyle:
                         AppTextStyles.montserratStyle(color: Colors.lightBlue),
                   ),
-                  TyperAnimatedText('Full Stack Developer',
+                  TyperAnimatedText(' Full Stack Developer',
                       textStyle: AppTextStyles.montserratStyle(
                           color: Colors.lightBlue)),
-                  TyperAnimatedText('Freelancer',
+                  TyperAnimatedText(' Freelancer',
                       textStyle: AppTextStyles.montserratStyle(
                           color: Colors.lightBlue))
                 ],
+                repeatForever: true, // Repeat the animation forever
                 pause: const Duration(milliseconds: 1000),
                 displayFullTextOnTap: true,
                 stopPauseOnTap: true,
@@ -112,13 +119,11 @@ class _HomePageState extends State<HomePage> {
         Constants.sizedBox(height: 15.0),
         FadeInDown(
           duration: const Duration(milliseconds: 1600),
-          child: Expanded(
-            child: Text(
-              ' I\'m very glad to let y\'all know about'
-              ' myself,my strengths'
-              ' and my projects that I\'ve completed in my portfolio.',
-              style: AppTextStyles.normalStyle(),
-            ),
+          child: Text(
+            'I\'m very glad to let y\'all know about'
+            ' myself, my strengths'
+            ' and my projects that I\'ve completed in my portfolio.',
+            style: AppTextStyles.normalStyle(),
           ),
         ),
         Constants.sizedBox(height: 22.0),
@@ -126,32 +131,31 @@ class _HomePageState extends State<HomePage> {
           duration: const Duration(milliseconds: 1600),
           child: SizedBox(
             height: 48,
-            child: ListView.separated(
-              itemCount: socialButtons.length,
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              separatorBuilder: (context, child) =>
-                  Constants.sizedBox(width: 8.0),
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {},
-                  onHover: (value) {
-                    setState(() {
-                      if (value) {
-                        socialBI = index;
-                      } else {
-                        socialBI = null;
+            child: Row(
+              children: [
+                for (var index = 0; index < socialButtons.length; index++)
+                  InkWell(
+                    onTap: () {
+                      if (index == 0) {
+                        _launchUrl(_linkedInUrl);
+                      } else if (index == 1) {
+                        _launchUrl(_instagramUrl);
+                      } else if (index == 2) {
+                        _launchUrl(_githubUrl);
                       }
-                    });
-                  },
-                  borderRadius: BorderRadius.circular(550.0),
-                  hoverColor: AppColors.themeColor,
-                  splashColor: AppColors.lawGreen,
-                  child: buildSocialButton(
-                      asset: socialButtons[index],
-                      hover: socialBI == index ? true : false),
-                );
-              },
+                    },
+                    borderRadius: BorderRadius.circular(550.0),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Image.asset(
+                        socialButtons[index],
+                        width: 45,
+                        height: 45,
+                        color: AppColors.themeColor,
+                      ),
+                    ),
+                  ),
+              ],
             ),
           ),
         ),
@@ -159,29 +163,19 @@ class _HomePageState extends State<HomePage> {
         FadeInUp(
           duration: const Duration(milliseconds: 1800),
           child: AppButtons.buildMaterialButton(
-              onTap: () {}, buttonName: 'Download CV'),
+            onTap: () {
+              _launchUrl(_url); // Call the function to launch the PDF link
+            },
+            buttonName: 'Download CV',
+          ),
         ),
       ],
     );
   }
 
-  Ink buildSocialButton({required String asset, required bool hover}) {
-    return Ink(
-      width: 45,
-      height: 45,
-      decoration: BoxDecoration(
-        border: Border.all(color: AppColors.themeColor, width: 2.0),
-        color: AppColors.bgColor,
-        shape: BoxShape.circle,
-      ),
-      padding: const EdgeInsets.all(6),
-      child: Image.asset(
-        asset,
-        width: 10,
-        height: 12,
-        color: hover ? AppColors.bgColor : AppColors.themeColor,
-        // fit: BoxFit.fill,
-      ),
-    );
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launch(url.toString())) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
